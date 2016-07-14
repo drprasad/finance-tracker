1d827d33303f81064ea3901272a3de7b533b148f
drprasad <tech.dr.prasad@live.com>
2016-07-13 22:49:22 -0400

    Add friendship model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160713152829) do
+ActiveRecord::Schema.define(version: 20160714024318) do
+
+  create_table "friend_ships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "stocks", force: :cascade do |t|
     t.string   "ticker"
